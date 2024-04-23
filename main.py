6ce83c22f33abe4553e9445a6bcc57587167fe34
pronisskor <163367282+pronisskor@users.noreply.github.com>
2024-04-23 12:30:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,19 @@ __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
-from langchain.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import RetrievalQA
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain.chains import RetrievalQA 
 import streamlit as st
 import tempfile
 import os
-from streamlit_extras.buy_me_a_coffee import button
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-button(username="jocoding", floating=True, width=221)
 
 #제목
 st.title("ChatPDF")
 st.write("---")
-
-#OpenAI KEY 입력 받기
-openai_key = st.text_input('OPEN_AI_API_KEY', type="password")
 
 #파일 업로드
 uploaded_file = st.file_uploader("PDF 파일을 올려주세요!",type=['pdf'])
@@ -51,20 +44,10 @@ if uploaded_file is not None:
     texts = text_splitter.split_documents(pages)
 
     #Embedding
-    embeddings_model = OpenAIEmbeddings(openai_api_key=openai_key)
+    embeddings_model = OpenAIEmbeddings()
 
     # load it into Chroma
     db = Chroma.from_documents(texts, embeddings_model)
-
-    #Stream 받아 줄 Hander 만들기
-    from langchain.callbacks.base import BaseCallbackHandler
-    class StreamHandler(BaseCallbackHandler):
-        def __init__(self, container, initial_text=""):
-            self.container = container
-            self.text=initial_text
-        def on_llm_new_token(self, token: str, **kwargs) -> None:
-            self.text+=token
-            self.container.markdown(self.text)
 
     #Question
     st.header("PDF에게 질문해보세요!!")
@@ -72,8 +55,7 @@ if uploaded_file is not None:
 
     if st.button('질문하기'):
         with st.spinner('Wait for it...'):
-            chat_box = st.empty()
-            stream_hander = StreamHandler(chat_box)
-            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=openai_key, streaming=True, callbacks=[stream_hander])
+            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
             qa_chain = RetrievalQA.from_chain_type(llm,retriever=db.as_retriever())
-            qa_chain({"query": question})
+            result = qa_chain({"query": question})
+            st.write(result["result"])
