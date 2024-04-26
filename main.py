7e5f52d1c2e2b4acc1172a775e177ebe8360d0fb
pronisskor <163367282+pronisskor@users.noreply.github.com>
2024-04-26 18:43:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
-from langchain.retrievers.multi_query import MultiQueryRetriever
 from langchain.chains import RetrievalQA
 import streamlit as st
 import tempfile
